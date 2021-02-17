56c157ba81551b1755f4f40efd92ad4e42731417
Polina Filippova <74130886+Polina172001@users.noreply.github.com>
2021-02-17 14:17:32 +0300

    Add files via upload

---
+++ task4.c/task4.c/task4.c.cpp
@@ -0,0 +1,26 @@
+﻿// task4.c.cpp 
+
+#include <iostream>
+
+using namespace std;
+
+void f(void);
+double x, y, res;
+
+void main()
+{
+	cout.precision(5);
+	cout << "Input x\n";
+	cin >> x;
+	cout << "Input y\n";
+	cin >> y;
+	f();
+	cout << "f = " << res;
+}
+
+void f(void)
+{
+	res = ((x - 1) * sqrt(x)) - ((y - 1) * sqrt(y)) / ((sqrt((pow(x, 3)) * y)) + (x * y) + pow(x, 2) - x);
+}
+
+
