28cb5ddea212696ff57f65b72ed0ccc182cbaf31
Polina Filippova <74130886+Polina172001@users.noreply.github.com>
2021-02-17 14:25:07 +0300

    Add files via upload

---
+++ task_7.cpp/task_7.cpp/task7_main.cpp
@@ -0,0 +1,17 @@
+﻿// task_7.cpp.cpp 
+
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+void main()
+{
+	cout.precision(5);
+	cout << "Input x\n";
+	cin >> x;
+	cout << "Input y\n";
+	cin >> y;
+	f();
+	cout << "f = " << res;
+}
